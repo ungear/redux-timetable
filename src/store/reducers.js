f84677ee09622bb62e6250d871918d7411b02388
ungear <redeemer@inbox.ru>
2018-05-09 23:08:24 +0300

    state scheme draft

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,7 +3,35 @@ import { combineReducers } from 'redux';
 function test(state = {name: 'aaa'}, action){
   return state;
 }
+/*
+  totals: {
+    days:{
+      data: {dayId1: 2, dayId2: 4},
+      ids: [dayId1, dayId2]
+    },
+    tasks: {
+      data: { taskId1: {}, taskId2: {}}
+      ids: [taskId1, taskId2]
+    },
+    overall: 999
+  },
+  tasks:{
+    data:{
+      1: {
+        name: "task name",
+        days:{
+          data: {dayId1: 2, dayId2: 4},
+          ids: [dayId1, dayId2]
+        }
+      },
+      2: {}
+    }
+    ids: [1,2 ...]
+  }
 
+
+
+*/
 const app = combineReducers({
   test,
 })
